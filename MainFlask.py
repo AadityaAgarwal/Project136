19b2f5bdbd0759e3447c008edbd87a67a985565c
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2021-09-14 18:43:43 +0530

    Create MainFlask.py

---
+++ MainFlask.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request 
+from ProjectData import data
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.route("/") 
+def index(): 
+    return jsonify({ "data": data, "message": "success" }), 200 
+@app.route("/star")
+def star(): 
+    name = request.args.get("name")
+    star_data = next(item for item in data if item["name"] == name)
+    return jsonify({ "data": star_data, "message": "success" }), 200
+if __name__ == "__main__": app.run()
